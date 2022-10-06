4a05f86cbc030d36bd908d52438f0df2718cc067
THARUN BALAJI <63972701+thaurnbalaji@users.noreply.github.com>
2022-10-06 15:37:09 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,6 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast menu')
+streamlit.text('omega3 and blueberry oatmeal')
+streamlit.text('Kale, spinach and rocket smoothie')
+streamlit.text('Hard-boiled-free-range-egg')
