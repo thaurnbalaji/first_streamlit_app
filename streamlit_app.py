008286afdb8f9afe2f2e84e32c57eee35f7e9fe8
THARUN BALAJI <63972701+thaurnbalaji@users.noreply.github.com>
2022-10-06 15:44:42 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,8 @@ streamlit.text('Kale, spinach and rocket smoothie')
 streamlit.text('Hard-boiled-free-range-egg')
 streamlit.text('Avacado toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
